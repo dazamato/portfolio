4398b00bd943950d846d029a592ad29b6da277fd
Azamat <dazamato@gmail.com>
2019-09-28 18:24:04 +0600

    pass minor changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),
-    # path('passwordchange', views.passwordchange, name='passwordchange'),
-    # path('passw/<str:uidb64>/<str:token>', views.password, name='pasword'),
     path('pasw/', include('password_reset.urls')),
     path('profile/(<str:username>', views.get_user_profile, name='profile'),
 ]
